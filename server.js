1b3260610e7faaebdc28381ada91b26985fa3ac3
Alisher Tungyshbay <www.at.7777@gmail.com>
2024-02-24 16:31:54 +0600

    extra v2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const favicon = require('serve-favicon');
 
 // hash variables
 const crypto = require('crypto');
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 
 
 // models
