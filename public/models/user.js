236ef9cee949ebe3791469998bdf2332019bf89b
guzalm <220930@astanait.edu.kz>
2024-02-28 02:23:21 +0600

    Update user.js

--- public/models/user.js
+++ public/models/user.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-   username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        default: 'user'
-    },
+  username: {
+    type: String,
+    //required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    //required: true
+  },
+  role: {
+    type: String,
+    default: 'user'
+  },
+  shoppingCart: [{
+    productName: String,
+    quantity: Number
+    // Add any other fields you need for your shopping cart items
+  }]
 }, { collection: 'users' });
 
 const User = mongoose.model('User', UserSchema);
